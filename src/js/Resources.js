abf47923b04a39d21c6a1e6b85ac843796a90413
Alex Balak <alex.balak@outlook.com>
2025-08-04 17:42:09 +0200

    Beginning to code

---
+++ src/js/Resources.js
@@ -0,0 +1,17 @@
+class Resources {
+  constructor() {
+    //EVERYTHING TO LOAD
+    this.toLoad = {
+      sky: "/sprites/sky.png",
+      ground: "/sprites/ground.png",
+      hero: "/sprites/hero-sheet.png",
+      shadow: "/sprites/shadow.png",
+    };
+    this.images = {};
+
+    Object.keys(this.toLoad).forEach((key) => {
+      const img = new Image();
+      img.src = this.toLoad[key];
+    });
+  }
+}
